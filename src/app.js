642a3eb08392ce5e58e329225318798666b72b9c
kim <yuyukgh@naver.com>
2022-03-11 00:56:54 +0900

    webpack5 asset/resource 테스트

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import "./app.css";
 import * as math from "./math";
+import img from "./test.png";
+import art from "./art.jpg";
 
 (function () {
   math.math(44);
--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,18 @@ module.exports = {
   output: {
     path: path.resolve("./dist"),
     filename: "[name].js",
+    assetModuleFilename: "images/[name][ext]",
+    clean: true, // hash로 인한 번들시 최신을 제외한 나머지 삭제
   },
   module: {
     rules: [
       {
         test: /\.css$/, // css형식을 읽는다.
         use: ["style-loader", "css-loader"], // 읽는 순서는 뒤에서부터 앞으로이다. 1.css -> 2.style style-loader는 css를 접목하며 css-loader는 css를 자바스크립트 모듈화한다.
+      },
+      {
+        test: /\.(png||jpg)$/, // png, jpg형식을 읽는다.
+        type: "asset/resource", // 파일을 내보내고 URL을 추출합니다 이전버젼 -> file-loader
       },
     ],
   },
