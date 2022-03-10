3dc1c94add9f6ff40c88139e8d097e48a9129029
kim <yuyukgh@naver.com>
2022-03-10 23:46:19 +0900

    module/css-loader, style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,12 @@ module.exports = {
     path: path.resolve("./dist"),
     filename: "[name].js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // css형식을 읽는다.
+        use: ["style-loader", "css-loader"], // 읽는 순서는 뒤에서부터 앞으로이다. 1.css -> 2.style style-loader는 css를 접목하며 css-loader는 css를 자바스크립트 모듈화한다.
+      },
+    ],
+  },
 };
